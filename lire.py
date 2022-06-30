f56b545c03306a25c1e9850ec16510440b77588e
thabe03 <xi1le7@gmail.com>
2022-06-30 16:22:54 -0400

    modif

--- lire.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('annonce.db',check_same_thread=False)
-cursor = conn.cursor()
-
-def lire():
-  cursor.execute("SELECT * FROM batisse")
-  row = cursor.fetchall()
-  return row
